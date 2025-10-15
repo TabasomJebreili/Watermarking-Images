c27c3c766c1790700257b5b04e7d2d6715b6ec94
Tabasom <tabasom.jebreili@gmail.com>
2025-10-15 20:02:08 +0330

    change the logic

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 from tkinter import *
-from PIL import Image, ImageTk
-import random
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+import tkinter.filedialog as fd
+from tkinter import colorchooser
 
 
-def add_text():
-    text_name = canvas.create_text(w // 2, h // 2, text='Write your text here', fill='white', font=('Courier New', 15, 'bold'))
-    return text_name
+def path():
+    root = fd.askopenfilename()
+    return root
+
+
+def save():
+    global img
+    save_address = fd.asksaveasfilename()
+    if path:
+        save_img = img.convert('RGB')
+        save_img.save(address)
+
+
+def rotate(direction):
+    global img
+    if direction == "right":
+        img = img.rotate(90)
+    if direction == "left":
+        img = img.rotate(-90)
+    return img
+
+
+def new_text(fnt, size, x, y, text, r, g, b, a):
+    global img
+    transparent_img = Image.new("RGBA", img.size, (255, 255, 255, 0))
+    draw = ImageDraw.Draw(transparent_img)
+    font = ImageFont.truetype(fnt, size)
+    draw.text((x, y), text, font=font, fill=(r, g, b, a))
+    img = Image.alpha_composite(img, transparent_img)
+    return img
+
+def transparency(value):
+    a = transparency_scale.get()
+    return a
+
+def choose_color():
+    color, hex_code = colorchooser.askcolor(title='Choose the Color:')
+    r = color[0]
+    g = color[1]
+    b = color[2]
+    return r, g, b
 
 
 window = Tk()
@@ -13,46 +52,18 @@ window.title('Watermark Your Image')
 window.minsize()
 window.configure(bg="black", pady=50, padx=50)
 
-MAX_WIDTH = 600
-MAX_HEIGHT = 500
-img = Image.open('Aurora_history-b1c66fb.jpg')
-img.thumbnail((MAX_WIDTH, MAX_HEIGHT))
-w, h = img.size
-photo = ImageTk.PhotoImage(img)
-
-canvas = Canvas(width=w, height=h, bg='black', highlightthickness=0)
-canvas.create_image(w//2, h//2, image=photo)
-canvas.grid(column=0, row=0, columnspan=2, rowspan=1)
-
-x_label = Label(text="Insert the x position of your text:", bg='black', fg="white", font=('Courier New', 12))
-x_label.grid(column=0, columnspan=1, row=2, sticky="w")
-
-x_place = Entry(width=8, bg='white', fg='black', font=('Courier New', 10))
-x_place.insert(END, 'e.g. 120')
-x_place.grid(column=1, row=2, sticky="w")
-
-y_label = Label(text="Insert the y position of your text:", bg='black', fg="white", font=('Courier New', 12))
-y_label.grid(column=0, columnspan=1, row=3, sticky="w")
-
-y_place = Entry(width=8, bg='white', fg='black', font=('Courier New', 10))
-y_place.insert(END, 'e.g. 120')
-y_place.grid(column=1, row=3, sticky="w")
-
-your_text_label = Label(text="Insert the text:", bg='black', fg="white", font=('Courier New', 12))
-your_text_label.grid(column=0, columnspan=1, row=4, sticky="w")
-
-your_text_entry = Entry(width=15, bg='white', fg='black', font=('Courier New', 10))
-your_text_entry.insert(END, 'Write your text here')
-your_text_entry.grid(column=1, columnspan=1, row=4, sticky="w")
-
-color_label = Label(text='Choose the color of your text:', bg='black', fg="white", font=('Courier New', 12))
-color_label.grid(column=0, columnspan=1, row=5, sticky="w")
+address = path()
+img = Image.open(address).convert('RGBA')
 
 
+select_file_button = (Button(text='Select image', bg="black", fg='white', font=('Helvetica', 12), command=path)
+                      .grid(column=0, row=0, columnspan=2))
 
-add_text_button = Button(text='Add Text', bg='black', fg='white', command=add_text, font=('Courier New', 14))
-add_text_button.config(pady=20)
-add_text_button.grid(column=0, row=6)
+transparency_label = Label(text='transparency', fg='white', bg="black")
+transparency_label.grid(column=0, row=1)
+transparency_scale = Scale(from_=0, to=255)
+transparency_scale.grid(column=1, row=1)
+
 
 
 
